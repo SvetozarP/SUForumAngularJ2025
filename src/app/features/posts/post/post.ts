eaf8d00be1854e70f0f8d0b6bd4bba10d8d6923d
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-07-20 17:04:02 +0100

    Updated changeDetectionStrategy for post and theme to onPush

--- src/app/features/posts/post/post.ts
+++ src/app/features/posts/post/post.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { PostModel } from '../../../models';
 
 @Component({
   selector: 'app-post',
   imports: [],
   templateUrl: './post.html',
-  styleUrl: './post.css'
+  styleUrl: './post.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Post {
   @Input() post!: PostModel;
--- src/app/features/themes/theme/theme.ts
+++ src/app/features/themes/theme/theme.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { ThemeModel } from '../../../models';
 
 @Component({
   selector: 'app-theme',
   imports: [],
   templateUrl: './theme.html',
-  styleUrl: './theme.css'
+  styleUrl: './theme.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class Theme {
   @Input() theme!: ThemeModel;
