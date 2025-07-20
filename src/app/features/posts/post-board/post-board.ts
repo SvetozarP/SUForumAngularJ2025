93788719d79401160ac879474c03523b3508f3d8
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-07-20 16:32:30 +0100

    Converted post-board to async capture posts, same logic as themes.

--- src/app/features/posts/post-board/post-board.ts
+++ src/app/features/posts/post-board/post-board.ts
@@ -1,29 +1,24 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { PostsService } from '../../../core/services';
 import { PostModel } from '../../../models';
 import { Post } from "../post/post";
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-post-board',
-  imports: [Post],
+  imports: [Post, CommonModule],
   templateUrl: './post-board.html',
   styleUrl: './post-board.css'
 })
-export class PostBoard implements OnInit, OnDestroy {
+export class PostBoard {
 
-  subscriptions: Subscription[] = [];
+  posts$: Observable<PostModel[]>;
   posts: PostModel[] = [];
 
-  constructor(private postsService: PostsService) {}
-
-  ngOnInit(): void {
-    this.subscriptions.push(this.postsService.getRecentPosts().subscribe((posts: PostModel[]) => {
-      this.posts = posts;
-    }));
+  constructor(private postsService: PostsService) {
+    this.posts$ = this.postsService.getRecentPosts();
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
-  }
+ 
 }
