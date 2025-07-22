e8eed2a83c543b8668e2b150b6ce72e26e9d74c6
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-07-22 21:04:44 +0100

    Created home component and route to home.

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/components';
 import { Footer } from "./shared/components/";
-import { ThemeBoard } from "./features/themes";
-import { PostBoard } from "./features/posts";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, ThemeBoard, PostBoard],
+  imports: [RouterOutlet, Header, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
