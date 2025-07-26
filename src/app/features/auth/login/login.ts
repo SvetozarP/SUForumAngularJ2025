08dadc2828d656ad736dfe4d056c20241888479b
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-07-26 21:55:21 +0100

    Migrated login form to Reactive Form

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,69 +1,83 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../core/services';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink, FormsModule],
+  imports: [ReactiveFormsModule, RouterLink, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
   private authService = inject(AuthService);
   private router = inject(Router);
+  private formBuilder = inject(FormBuilder);
 
-  email: string = '';
-  password: string = '';
-  emailError: boolean = false;
-  passwordError: boolean = false;
-  emailErrorMessage: string = '';
-  passwordErrorMessage: string = '';
+  loginForm: FormGroup;
 
-  validateEmail(): void {
-    if(!this.email) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is required!';
-    } else if(!this.isValidEmail(this.email)) {
-      this.emailError = true;
-      this.emailErrorMessage = 'Email is not valid';
+  constructor() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(5)]]
+    });
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  get emailErrorMessage(): string {
+    if(this.email?.errors?.['required']) {
+      return 'Email is required!';
+    } else if(this.email?.errors?.['email']) {
+      return 'Email is not valid';
     } else {
-      this.emailError = false;
-      this.emailErrorMessage = '';
+      return '';
     }
   }
 
-  validatePassword(): void {
-    if(!this.password) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password is required!';
-    } else if (this.password.length < 4) {
-      this.passwordError = true;
-      this.passwordErrorMessage = 'Password must be at least 4 characters!';
+  get passwordErrorMessage(): string {
+    if(this.password?.errors?.['required']) {
+      return 'Password is required!';
+    } else if(this.password?.errors?.['minlength']) {
+      return 'Password must be at least 5 characters!';
     } else {
-      this.passwordError = false;
-      this.passwordErrorMessage = '';
+      return '';
     }
   }
 
-  isFormValid(): boolean {
-    return !this.emailError && !this.passwordError && Boolean(this.email) && Boolean(this.password);
+  get emailError(): boolean {
+    return this.email?.invalid && (this.email?.touched || this.email?.dirty) || false;
+  }
+
+  get passwordError(): boolean {
+    return this.password?.invalid && (this.password?.touched || this.password?.dirty) || false;
   }
 
   onSubmit(): void {
-    this.validateEmail();
-    this.validatePassword();
-    if(this.isFormValid()) {
-      const response = this.authService.login(this.email, this.password);
+
+    if(this.loginForm.valid) {
+      const [email, password] = this.loginForm.value;
+      const response = this.authService.login(email, password);
       if(response === true) {
         this.router.navigate(['/home']);
       } else {
-        this.emailError = true;
-        this.passwordError = true;
-        this.emailErrorMessage = 'Invalid email or password!';
-        this.passwordErrorMessage = 'Invalid email or password!';
+        this.markFormGroupTouched();
       }
     }
+  }
+  
+  private markFormGroupTouched(): void {
+    Object.keys(this.loginForm.controls).forEach(field => {
+      const control = this.loginForm.get(field);
+      if(control) {
+        control?.markAsTouched();
+      }
+    });
   }
 
   private isValidEmail(email: string): boolean {
