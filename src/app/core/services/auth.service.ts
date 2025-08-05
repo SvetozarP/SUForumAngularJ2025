81506967a59e57115d23e3f85a7f1caf72f6a80e
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-08-05 21:58:10 +0100

    Modified AuthService to work with current node server

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,24 +1,21 @@
 import { Injectable, signal } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { User } from "../../models";
+import { ApiUser, User } from "../../models";
+import { map, Observable, tap } from "rxjs";
 
 @Injectable({
     providedIn: 'root'
 })
 export class AuthService {
 
+    private apiUrl = 'http://localhost:3000/api';
     private _isLoggedIn = signal<boolean>(false);
     private _currentUser = signal<User | null>(null);
-    private _users: User[] = [
-        {id: '5fa64b162183ce1728ff371d', username: 'John', email: 'john@gmail.com', phone: '885 888 888'},
-        {id: '5fa64b162183ce1728ff371e', username: 'Jane', email: 'jane@gmail.com', phone: '885 888 888'},
-        {id: '5fa64b162183ce1728ff371f', username: 'David', email: 'david@gmail.com', phone: '885 888 888'},
-    ];
 
     public isLoggedIn = this._isLoggedIn.asReadonly();
     public currentUser = this._currentUser.asReadonly();
 
-    constructor() {
+    constructor(private httpClient: HttpClient) {
         const savedUser = localStorage.getItem('currentUser');
         if (savedUser) {
             const user: User = JSON.parse(savedUser);
@@ -27,50 +24,81 @@ export class AuthService {
         }
      }
 
-    login(email: string, password: string): boolean {
-        if(email && password) {
-            const user = this._users[0];
-            this._currentUser.set(user);
-            this._isLoggedIn.set(true);
-            localStorage.setItem('currentUser', JSON.stringify(user));
-            return true;
-        }
-        return false;
+    login(email: string, password: string): Observable<User> {
+        return this.httpClient.post<ApiUser>(`${this.apiUrl}/login`, {email, password}, {
+            withCredentials: true
+        }).pipe(
+            map(apiUser => this.mapApiUserToUser(apiUser)),
+            tap(user => {
+                this._currentUser.set(user);
+                this._isLoggedIn.set(true);
+                localStorage.setItem('currentUser', JSON.stringify(user));
+            })
+        );
+        
     }
 
-    register(username: string, email: string, phone: string, password: string, rePassword: string): boolean {
-        if(username && email && phone &&password && rePassword) {
-            const newUser: User = {
-                id: `user_${Date.now()}`,
-                username,
-                email,
-                phone
-            };
-            this._users.push(newUser);
-            this._currentUser.set(newUser);
-            this._isLoggedIn.set(true);
-            localStorage.setItem('currentUser', JSON.stringify(newUser));
-            return true;
-        }
-        return false;
+    register(username: string, email: string, phone: string, password: string, rePassword: string): Observable<User> {
+        return this.httpClient.post<ApiUser>(`${this.apiUrl}/register`, {
+            username, 
+            email, 
+            tel: phone, 
+            password, 
+            rePassword
+        }, {
+            withCredentials: true
+        }).pipe(
+            map(apiUser => this.mapApiUserToUser(apiUser)),
+            tap(user => {
+                this._currentUser.set(user);
+                this._isLoggedIn.set(true);
+                localStorage.setItem('currentUser', JSON.stringify(user));
+            })
+        );
     }
 
-    logout(): void {
-        this._currentUser.set(null);
-        this._isLoggedIn.set(false);
-        localStorage.removeItem('currentUser');
+
+    logout(): Observable<void> {
+        return this.httpClient.post<void>(`${this.apiUrl}/logout`, {}, {
+            withCredentials: true
+        }).pipe(
+            tap(() => {
+                this._currentUser.set(null);
+                this._isLoggedIn.set(false);
+                localStorage.removeItem('currentUser');
+            })
+        );
     }
 
     getCurrentUserID(): string | null {
         return this._currentUser()?.id || null;
     }
 
-    updateUser(id: string | undefined, user: User): void {
-        const index = this._users.findIndex(u => u.id === id);
-        if(index !== -1) {
-            this._users[index] = user;
-        }
-        this._currentUser.set(user);
-        localStorage.setItem('currentUser', JSON.stringify(user));
+    updateUser(user: User): Observable<User> {
+        const apiUser = <ApiUser> {
+            _id: user.id,
+            username: user.username,
+            email: user.email,
+            tel: user.phone
+        };
+
+        return this.httpClient.put<ApiUser>(`${this.apiUrl}/users/${user.id}`, apiUser, {
+            withCredentials: true
+        }).pipe(
+            map(apiUser => this.mapApiUserToUser(apiUser)),
+            tap(user => {
+                this._currentUser.set(user);
+                localStorage.setItem('currentUser', JSON.stringify(user));
+            })
+        );
+    }
+
+    private mapApiUserToUser(apiUser: ApiUser): User {
+        return <User> {
+            id: apiUser._id,
+            username: apiUser.username,
+            email: apiUser.email,
+            phone: apiUser.tel
+        };
     }
 }
