506bab9935a9814d26766dd954c8a520dbbdadad
SvetozarP <145108913+SvetozarP@users.noreply.github.com>
2025-08-06 22:08:13 +0100

    Implemented error notification service

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './shared/components';
+import { Header, ErrorNotification } from './shared/components';
 import { Footer } from "./shared/components/";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, ErrorNotification],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
